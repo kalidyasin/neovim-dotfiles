ce0b5c10aaa2b072ee80397daccf45d62b8d0a25
Kalid Yasin <kalidyasin29@gmail.com>
2025-04-25 01:23:44 +0300

    first commit
    Signed-off-by: Kalid Yasin <kalidyasin29@gmail.com>

---
+++ lua/plugins/file-manager.lua
@@ -0,0 +1,12 @@
+return {
+  "stevearc/oil.nvim",
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {},
+  -- Optional dependencies
+  dependencies = { { "echasnovski/mini.icons", opts = {} } },
+  lazy = false,
+  key = {
+      vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" }),
+  } 
+}
