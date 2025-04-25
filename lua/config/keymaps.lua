8b5f42197965bf717803d5e96bcc9f13d19e7701
Kalid Yasin <kalidyasin29@gmail.com>
2025-04-26 02:44:51 +0300

    add lsp

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,11 @@
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
--- lua/plugins/which-keys.lua
+++ lua/plugins/which-keys.lua
@@ -45,8 +45,11 @@ return {
 
     -- Document existing key chains
     spec = {
+      { '<leader>c', group = '[C]ode' },
       { '<leader>s', group = '[S]earch' },
       { '<leader>t', group = '[T]oggle' },
+      { '<leader>d', group = '[D]ocument' },
+      { '<leader>w', group = '[W]orkspace' },
       { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
     },
   },
