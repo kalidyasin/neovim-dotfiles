e125ed69bcaf3cde307afb6b8d2acf05a1139ce4
Kalid Yasin <kalidyasin29@gmail.com>
2025-04-25 17:23:22 +0300

    add: keymaps

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -23,17 +23,19 @@ return {
   keys = {
     { "<leader>lg", function() Snacks.lazygit() end, desc = "[L]azy[G]it" },
     { "<leader>un", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
-    { "<c-/>",      function() Snacks.terminal() end, desc = "Toggle Terminal" },
+    { "<c-/>",      function() Snacks.terminal() end, desc = "Toggle Terminal", mode = {"n", "t"} },
     { "<c-_>",      function() Snacks.terminal() end, desc = "which_key_ignore" },
     { "]]",         function() Snacks.words.jump(vim.v.count1) end, desc = "Next Reference", mode = { "n", "t" } },
     { "[[",         function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference", mode = { "n", "t" } },
 
     -- Picker key maps
     { "<leader>ss", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
-    { "<leader><space>", function() Snacks.picker.buffers() end, desc = "Buffers" },
+    { "<leader><space>", function() Snacks.picker.buffers() end, desc = "[ ] Find existing buffers" },
     { "\\", function() Snacks.explorer() end, desc = "File Explorer" },
-    { "<leader>sg", function() Snacks.picker.grep() end, desc = "Grep" },
+    { "<leader>sg", function() Snacks.picker.grep() end, desc = "[S]earch by [G]rep" },
     { "<leader>sn", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find Config File" },
-    { "<leader>sf", function() Snacks.picker.files() end, desc = "Find Files" },
+    { "<leader>sf", function() Snacks.picker.files() end, desc = "[S]earch [F]iles" },
+    { "<leader>sp", function() Snacks.picker.pickers() end, desc = "[S]earch [P]ickers" },
+    { "<leader>s.", function() Snacks.picker.recent() end, desc = "[S]earch Recent Files ('.' for repeat)" },
   }
 }
