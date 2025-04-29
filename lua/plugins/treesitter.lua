20fb0fe26669df6036acb6d4f2f7c39fbbbc6e62
Kalid Yasin <kalidyasin29@gmail.com>
2025-04-29 12:30:32 +0300

    fix conflict with enter key

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -21,8 +21,8 @@ return {
         incremental_selection = {
             enable = true,
             keymaps = {
-                init_selection = "<Enter>", -- set to `false` to disable one of the mappings
-                node_incremental = "<Enter>",
+                init_selection = "<M-space>", -- set to `false` to disable one of the mappings
+                node_incremental = "<M-space>",
                 scope_incremental = false,
                 node_decremental = "<Backspace>",
             },
