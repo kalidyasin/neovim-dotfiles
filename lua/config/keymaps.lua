182b190aa163369b69f3882d90db40e05982b243
Kalid Yasin <kalidyasin29@gmail.com>
2026-01-19 04:22:31 +0300

    feat: add more usefull keymmaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -26,6 +26,20 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move selected lines down' })
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move selected lines up' })
 
+-- Keep cursor in place when joining lines
+vim.keymap.set('n', "J", "mzJ`z", { desc = 'Join lines' })
+
+-- Keep cursor in place when scrolling
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Scroll down half page' })
+vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Scroll up half page' })
+
+-- Keep cursor in place when searching
+vim.keymap.set('n', 'n', 'nzzzv', { desc = 'Next search result' })
+vim.keymap.set('n', 'N', 'Nzzzv', { desc = 'Previous search result' })
+
+-- Paste without yanking
+vim.keymap.set('x' , '<leader>p', '"_dP', { desc = 'Paste without yanking' })
+
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
 --
